5c68ab68346e77e4d9c7705143cd63738c12f100
Pavel Rusinov <pa1603ru@rambler.ru>
2012-09-18 17:42:21 +0400

    homework

---
+++ java/Palindrome/src/palindrome/PalindromeCaseIndep.java
@@ -0,0 +1,48 @@
+
+package palindrome;
+
+public class PalindromeCaseIndep extends Palindrome
+{
+    @Override
+    public boolean isPalindrome(String str) 
+    {
+        str = str.toLowerCase();
+        
+        int fromBeg;
+	int fromEnd;
+	boolean result;
+	
+	fromEnd = str.length() - 1;
+	fromBeg = 0;
+	result = true;
+	
+	while(fromEnd > fromBeg && result == true)
+	{
+		if(Character.isLetter(str.charAt(fromEnd)) == false)
+		{
+			fromEnd = fromEnd - 1;
+		}
+	
+		if(Character.isLetter(str.charAt(fromBeg)) == false)
+		{
+			fromBeg = fromBeg + 1;
+		}
+		
+		if(Character.isLetter(str.charAt(fromEnd)) != false && Character.isLetter(str.charAt(fromBeg)) != false)
+		{
+			if(str.charAt(fromEnd) != str.charAt(fromBeg))
+			{
+				result = false;
+			}
+			
+			else
+			{
+				fromEnd = fromEnd-1;
+				fromBeg = fromBeg+1;
+			}
+		}
+	}
+	
+	return result;
+    }
+}
---
+++ java/World/src/world/Main.java
@@ -0,0 +1,8 @@
+package world;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
